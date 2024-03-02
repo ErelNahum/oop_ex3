b9f57ad5e2ae67e2ad0044a2fe77e0d1f42f9b36
ErelNahum <erelnahum2003@gmail.com>
2024-03-02 21:51:12 +0200

    Showing full charset - (forum question)

--- src/image_char_matching/SubImgCharMatcher.java
+++ src/image_char_matching/SubImgCharMatcher.java
@@ -112,8 +112,8 @@ public class SubImgCharMatcher {
      *
      * @return the list
      */
-    public List<Character> getCharSet() {
-        return charSet.stream().toList();
+    public SortedSet<Character> getCharSet() {
+        return charSet;
     }
 }
 
